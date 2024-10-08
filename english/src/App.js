f12e9c5d57685e80ef3f9e8c5c70fa0c6d96d9a4
VuykoV <VuykoVlad@ukr.net>
2024-10-08 18:34:02 +0200

    first commit

---
+++ english/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <h1>Test</h1>
+          <p>This text is just a test and will be removed later</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
